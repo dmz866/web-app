5170a4285604ac636df3f37896e8986351aafa6d
David Murillo <dmz866@hotmail.com>
2025-03-06 20:55:11 -0500

    tarea crud views added

--- src/api/base/views.py
+++ src/api/base/views.py
@@ -1,7 +1,7 @@
 from django.shortcuts import render
 from django.http import HttpResponse
 from django.urls import reverse_lazy
-from django.views.generic import ListView, DetailView, CreateView
+from django.views.generic import ListView, DetailView, CreateView, UpdateView, DeleteView
 
 from .models import Tarea
 
@@ -21,4 +21,14 @@ class DetalleTarea(DetailView):
 class CrearTarea(CreateView):
     model = Tarea
     fields = '__all__' #Show all fields
+    success_url = reverse_lazy('tareas')
+
+class EditarTarea(UpdateView):
+    model = Tarea
+    fields = '__all__' #Show all fields
+    success_url = reverse_lazy('tareas')
+
+class EliminarTarea(DeleteView):
+    model = Tarea
+    context_object_name = 'tarea'
     success_url = reverse_lazy('tareas')
